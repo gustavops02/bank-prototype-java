ac1c240f917602bbb9d3e83b0dc0547c576d8d8d
Gustavo Paulo <gustavo.psilvaa02@gmail.com>
2023-06-08 00:08:35 -0300

    Design for CLI usage has been started

--- src/main/java/com/bank/Main.java
+++ src/main/java/com/bank/Main.java
@@ -1,8 +1,24 @@
 package com.bank;
 
 
+import com.bank.utilitarios.DesignConfig;
+
+import java.util.Scanner;
+
+import static com.bank.utilitarios.DesignConfig.layout;
+
 public class Main {
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+
+        DesignConfig.layout();
+
     }
+
+
+
+
+
+
 }
---
+++ src/main/java/com/bank/utilitarios/DesignConfig.java
@@ -0,0 +1,23 @@
+package com.bank.utilitarios;
+
+import java.sql.ResultSet;
+
+public class DesignConfig {
+
+    public static void layout() {
+
+        System.out.print("Bem-vindo ao Sistema Bancário!\n\n");
+        System.out.println("Nossas ferramentas:");
+        System.out.println("--------------------");
+        System.out.println("1 - Cadastrar conta");
+        System.out.println("2 - Consultar saldo");
+        System.out.println("3 - Efetuar depósito");
+        System.out.println("4 - Efetuar saque");
+        System.out.println("--------------------");
+        System.out.print("Escolha uma opção: ");
+    }
+
+    public static void resultado() {
+
+    }
+}
