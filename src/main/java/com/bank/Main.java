289c2134a2577202f066f307d55e7aafdecccf23
Gustavo Paulo <gustavo.psilvaa02@gmail.com>
2023-06-10 19:34:28 -0300

    Boilerplate partial remove from main class

--- src/main/java/com/bank/Main.java
+++ src/main/java/com/bank/Main.java
@@ -2,10 +2,9 @@ package com.bank;
 
 import com.bank.database.excecoes.DBException;
 import com.bank.entidades.Conta;
-import com.bank.entidades.ContaCorrente;
-import com.bank.entidades.ContaPoupanca;
-import com.bank.entidades.Pessoa;
+
 import com.bank.servicos.ServicoContas;
+import com.bank.servicos.ServicoTransacoes;
 import com.bank.utilitarios.DesignConfig;
 
 
@@ -29,31 +28,7 @@ public class Main {
 
         switch (numero) {
             case 1:
-                System.out.print("Nome: ");
-                String nome = sc.nextLine();
-
-                System.out.print("CPF: ");
-                String cpf = sc.nextLine();
-
-                Pessoa pessoa = new Pessoa(cpf, nome);
-
-                DesignConfig.tipoDeConta();
-                System.out.print("Escolha uma opção: ");
-                String tipo = sc.next().toUpperCase();
-
-                System.out.print("Irá fazer depósito no ato do cadastro?\nSe sim, coloque o valor: ");
-                Double saldo = sc.nextDouble();
-
-                if (tipo.equals("CP")) {
-                    ServicoContas.cadastrarConta(new ContaPoupanca(pessoa, saldo));
-                    System.out.println("Conta cadastrada!");
-
-                } else if (tipo.equals("CC")) {
-                    ServicoContas.cadastrarConta(new ContaCorrente(pessoa, saldo));
-
-                } else {
-                    System.out.println("erro");
-                }
+                ServicoTransacoes.executarCadastro(sc);
                 break;
             case 2:
                 System.out.print("--> Número da conta: ");
@@ -71,6 +46,9 @@ public class Main {
                 break;
         }
 
+    }
+
+    public static void cadastro() {
 
     }
 }
--- src/main/java/com/bank/servicos/ServicoTransacoes.java
+++ src/main/java/com/bank/servicos/ServicoTransacoes.java
@@ -1,6 +1,41 @@
 package com.bank.servicos;
 
 
+import com.bank.entidades.ContaCorrente;
+import com.bank.entidades.ContaPoupanca;
+import com.bank.entidades.Pessoa;
+import com.bank.utilitarios.DesignConfig;
+
+import java.util.Scanner;
+
 public class ServicoTransacoes {
 
+    public static void executarCadastro(Scanner sc) {
+
+        System.out.print("Nome: ");
+        String nome = sc.nextLine();
+
+        System.out.print("CPF: ");
+        String cpf = sc.nextLine();
+
+        Pessoa pessoa = new Pessoa(cpf, nome);
+
+        DesignConfig.tipoDeConta();
+        System.out.print("Escolha uma opção: ");
+        String tipo = sc.next().toUpperCase();
+
+        System.out.print("Irá fazer depósito no ato do cadastro?\nSe sim, coloque o valor: ");
+        Double saldo = sc.nextDouble();
+
+        if (tipo.equals("CP")) {
+            ServicoContas.cadastrarConta(new ContaPoupanca(pessoa, saldo));
+            System.out.println("Conta cadastrada!");
+
+        } else if (tipo.equals("CC")) {
+            ServicoContas.cadastrarConta(new ContaCorrente(pessoa, saldo));
+
+        } else {
+            System.out.println("erro");
+        }
+    }
 }
